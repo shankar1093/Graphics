b699a4d33daaa088207d637e385f493f9abc4876
Shankar Rao <shankar1093@gmail.com>
2015-12-02 20:10:49 -0500

    edited the server file to serve files from public dir

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Test application for front end in computer graphics')
-})
+require('./app/routes')(app)
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// app.get('/', function(request, response) {
+//   response.send('Test application for front end in computer graphics')
+// })
+
+
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'))
+// })
+
+
+app.listen(port);
+console.log("Magic happens on port" + port);
+exports = module.exports = app;
